de5f8b4109fbe313c0e3e4c74aa4e526b8111762
David THIBAU <david.thibau@gmail.com>
2024-07-16 20:09:03 +0200

    ScreenShot saved in Screenshots

--- src/test/java/MySeleniumTest.java
+++ src/test/java/MySeleniumTest.java
@@ -1,13 +1,25 @@
+import io.qameta.allure.Allure;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.TestWatcher;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@ExtendWith(MySeleniumTest.ScreenshotOnFailure.class)
 public class MySeleniumTest {
 
     private WebDriver driver;
@@ -29,13 +41,45 @@ public class MySeleniumTest {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        assertEquals("Gestion du catalogue produit", driver.getTitle());
+        assertEquals("Gestion du catalogue ", driver.getTitle());
     }
 
-    @AfterEach
+   /* @AfterEach
     public void tearDown() {
         if (driver != null) {
             driver.quit();
+        }
+    }*/
+
+    public static class ScreenshotOnFailure implements TestWatcher {
+        @Override
+        public void testFailed(ExtensionContext context, Throwable cause) {
+            WebDriver driver = ((MySeleniumTest) context.getRequiredTestInstance()).driver;
+            if (driver != null) {
+                File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+                String timestamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+                try {
+                    Files.copy(screenshot.toPath(), Paths.get("screenshots", "screenshot-" + timestamp + ".png"));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                driver.quit();
+            }
+        }
+
+        @Override
+        public void testSuccessful(ExtensionContext context) {
+            // Optional: Code to run when a test is successful
+        }
+
+        @Override
+        public void testAborted(ExtensionContext context, Throwable cause) {
+            // Optional: Code to run when a test is aborted
+        }
+
+        @Override
+        public void testDisabled(ExtensionContext context, Optional<String> reason) {
+            // Optional: Code to run when a test is disabled
         }
     }
 }
